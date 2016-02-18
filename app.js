087a1f717a3a31a1ff7427cef94b19ff0d57c17f
Greg Pfaff <greg.pfaff@razorfish.com>
2016-02-17 19:38:32 -0600

    adding chat app functionality

--- app.js
+++ app.js
@@ -11,10 +11,23 @@ io.on('connection', function(socket){
 })*/
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile('index.html');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
