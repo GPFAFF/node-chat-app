a06a149ec89c9c281c4af73e5ecfd843ebdeb4eb
Greg Pfaff <greg.pfaff@razorfish.com>
2016-02-17 20:22:50 -0600

    adding styling

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-/*var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000
-app.get('/', function(req, res){
-	res.sendFile(dirname + '/index.html');
-});
-io.on('connection', function(socket){
-	socket.on()
-})*/
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile('index.html');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
-
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
