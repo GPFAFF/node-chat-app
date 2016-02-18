1fa3d966887ef30283b478aa8a3c1d3a5860e82e
Greg Pfaff <greg.pfaff@razorfish.com>
2016-02-17 18:46:19 -0600

    adding chat app

---
+++ app.js
@@ -0,0 +1,21 @@
+/*var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000
+app.get('/', function(req, res){
+	res.sendFile(dirname + '/index.html');
+});
+io.on('connection', function(socket){
+	socket.on()
+})*/
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
